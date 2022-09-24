2b88ea27cbf98f2fe3915bc9bfcb48c9945ad2ed
altnskmuhammed <altnskmuh23@gmail.com>
2022-09-24 21:58:04 +0300

    new

---
+++ Electronic Arts/js/header.js
@@ -0,0 +1,16 @@
+function question() {
+  var x = document.getElementById("question");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function user() {
+  var y = document.getElementById("user");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
